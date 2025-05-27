583ed9afd86cceb0dfb74fbb556b51d86d436503
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-05-26 21:49:38 -0300

    Refatorando tabuada do 5 para remover código repetido

--- ex004.js
+++ ex004.js
@@ -4,37 +4,9 @@
 const tabuada = 5;
 
 for (let i = 1; i <= 10; i++) {
-    if (i == 1) {
-        result = tabuada * 1;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 2) {
-        result = tabuada * 2;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 3) {
-        result = tabuada * 3;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 4) {
-        result = tabuada * 4;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 5) {
-        result = tabuada * 5;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 6) {
-        result = tabuada * 6;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 7) {
-        result = tabuada * 7;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 8) {
-        result = tabuada * 8;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 9) {
-        result = tabuada * 9;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    } else if (i == 10) {
-        result = tabuada * 10;
-        console.log(`${tabuada} * ${i} = ${result}`)
-    }
+    const result = tabuada * 1;
+    console.log(`${tabuada} * ${i} = ${result}`);
+
 };
  
             
