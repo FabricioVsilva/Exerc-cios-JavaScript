cc6620b3e9af924eac8dd7c3d263151aea8dd35e
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-05-27 13:25:10 -0300

    Melhorando o código

--- ex004.js
+++ ex004.js
@@ -4,9 +4,8 @@
 const tabuada = 5;
 
 for (let i = 1; i <= 10; i++) {
-    const result = tabuada * 1;
+    const result = tabuada * i;
     console.log(`${tabuada} * ${i} = ${result}`);
-
 };
  
             
