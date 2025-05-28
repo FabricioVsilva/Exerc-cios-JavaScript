59003ac710188fe85ba49e1d676de49ce59e46fe
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-05-28 12:43:20 -0300

    Verificar se um número é positivo, negativo ou zero

---
+++ ex005.js
@@ -0,0 +1,9 @@
+// Verificar se um número é positivo, negativo ou zero
+
+const numero = 0
+
+if (numero === 0) {
+    console.log(`${numero} é zero!`)
+} else if (numero > 0) {
+    console.log(`${numero} é positivo`)
+} else(console.log(`${numero} negativo`))
