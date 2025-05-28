882a0d14ff775187751ee21c42e778f881195d10
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-05-28 13:34:06 -0300

    Concatenar nome e sobrenome e imprimir uma mensagem na tela

---
+++ ex008.js
@@ -0,0 +1,8 @@
+// Concatenar nome e sobrenome e imprimir mensagem de boas-vindas
+
+const nome = 'Fabrício Vieira';
+const sobrenome = 'Silva';
+
+const nome_completo = nome + ' ' + sobrenome
+
+console.log(`Seja bem vindo ${nome_completo}`)
