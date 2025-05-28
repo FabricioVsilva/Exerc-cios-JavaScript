8c64b659f8bda0bf387db8f067464515007aa9f8
bowie <marketing@letscode.com.br>
2025-05-28 20:44:59 -0300

    Verificar se idade é maior ou menor que 18 anos

---
+++ ex009.js
@@ -0,0 +1,10 @@
+// Verificar se uma idade é maior ou menor de idade (18 anos)
+
+const idade = 11;
+
+if (idade >= 18) {
+    console.log(`Você tem ${idade} anos de idade e já é maior de idade!`)
+} else {
+    console.log(`Você tem ${idade} anos de idade e ainda é menor de idade!`)
+}
+
