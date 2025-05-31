c41dbe23b749d625a533de159d122efa5bba78b1
bowie <marketing@letscode.com.br>
2025-05-31 16:39:04 -0300

    Adicionando comentários ao código

--- ex012.js
+++ ex012.js
@@ -1,4 +1,4 @@
-// Jogo de adivinhação entre 1 e 50.
+// Jogo de adivinhação entre 1 e 20.
 
 const readline = require('readline');
 
@@ -7,14 +7,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const numero_gerado = Math.floor(Math.random() * 50) + 1; // entre 1 e 50
+/*
+- Math.random() gera um número entre 0 e 1 (excluindo o 1).
+- Multiplicando por 50 → número entre 0 e 49.999...
+- Math.floor() arredonda **para baixo** → resultado vai de 0 a 49.
+- Somando +1 → final: número entre 1 e 50.
+- Esse é o número secreto que o usuário precisa adivinhar.
+*/
+const numero_gerado = Math.floor(Math.random() * 20) + 1;
 
 function pergunta_usuario() {
-    rl.question('Digite um número entre 1 e 50: ', function(resposta) {
-        const palpite = Number(resposta);
+    rl.question('Digite um número entre 1 e 20: ', function(resposta) {
+        const palpite = Number(resposta); // A resposta do usuário vem como texto (string), então aqui converti para número.
 
         if (palpite === numero_gerado) {
             console.log(`Parabéns! Você acertou o número ${numero_gerado}`);
+            rl.close();
         } else if (palpite < numero_gerado) {
             console.log(`Número abaixo que o gerado!`)
             pergunta_usuario();
@@ -25,4 +33,4 @@ function pergunta_usuario() {
     });
 }
 
-pergunta_usuario();
+pergunta_usuario(); // Chamando a função
