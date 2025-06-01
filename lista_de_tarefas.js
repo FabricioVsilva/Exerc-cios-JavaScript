e4c201a0b05c0ee36833d024e949e54a4b287976
bowie <marketing@letscode.com.br>
2025-06-01 14:39:43 -0300

    Finalizando projeto de lista de tarefas

--- lista_de_tarefas.js
+++ lista_de_tarefas.js
@@ -5,7 +5,7 @@ rl = readline.createInterface({
     output: process.stdout
 });
 
-let tarefas = []; // Array vazio onde posso guardar 
+let tarefas = [];
 
 function mostrarMenu() {
     console.log(`
@@ -17,5 +17,49 @@ function mostrarMenu() {
         4 - Sair
         ============================
         `);
-    }
+
+         rl.question('Escolha uma opção: ', function(opcao) {
+        if (opcao === '1') {
+            rl.question('Digite uma nova tarefa: ', function(tarefa) {
+                tarefas.push(tarefa);
+                console.log('Tarefa adicionada!');
+                mostrarMenu();
+            });
+
+        } else if(opcao === '2') {
+            console.log('\n=== TAREFAS ===');
+            tarefas.forEach((t, i) => {
+                console.log(`${i + 1}. ${t}`);
+            });
+            mostrarMenu();
+        } else if (opcao === '3') {
+            if (tarefas.length === 0) {
+                console.log('Nenhuma tarefa para remover.');
+                return mostrarMenu();
+            }
+
+            tarefas.forEach((t, i) => {
+                console.log(`${i + 1}. ${t}`);
+            });
+            rl.question('Digite o número da tarefa que deseja remover: ', function(numero) {
+              const index = parseInt(numero) -1;
+              if (index >= 0 && index < tarefas.length) {
+                tarefas.splice(index, 1);
+                console.log('Tarefa removida!');
+              }  
+              mostrarMenu();
+            });
+
+        }else if (opcao === '4') {
+            console.log('Saindo...');
+            rl.close();
+        } else {
+            console.log('Opção inválida!');
+            mostrarMenu();
+        }
+    });
+
+}
 mostrarMenu();
+
+       
