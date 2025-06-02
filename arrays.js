95ac30e7a911b3d7751abe7c9c4478fb61c39032
bowie <marketing@letscode.com.br>
2025-06-01 21:00:08 -0300

    Entendendo melhor o Array

---
+++ arrays.js
@@ -0,0 +1,8 @@
+// PRATICAR ARRAYS
+
+let tarefa = ['Estudar inglês', 'Estudar violino', 'Praticar matemática']
+
+tarefa.forEach((tarefa, indice) => {
+    console.log(`${indice + 1}. ${tarefa}`)
+});
+
