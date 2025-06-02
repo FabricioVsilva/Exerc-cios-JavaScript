415298d234b3461f8dee27efaffe5d593b3dc233
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-06-02 12:59:40 -0300

    Refatorando com implementação de function

--- ex003.js
+++ ex003.js
@@ -1,5 +1,5 @@
 // Par ou ímpar
-
+/*
 const a = 100;
 
 if (a % 2 === 0) {
@@ -7,3 +7,24 @@ if (a % 2 === 0) {
 } else{
     console.log("Número ímpar")
 };
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function parimpar() {
+    rl.question('Digite um número para saber se é par ou ímpar: ', (resposta) => {
+        const numero = Number(resposta);
+        if (resposta % 2 === 0) {
+            console.log(`${resposta} é par`);
+        } else {
+            console.log(`${resposta} é ímpar`)
+        }
+        rl.close();
+    });
+}
+parimpar();
