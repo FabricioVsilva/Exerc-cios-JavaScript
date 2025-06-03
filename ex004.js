99122a61822ed34f277448398e004398a54c08a8
bowie <marketing@letscode.com.br>
2025-06-02 22:52:47 -0300

    Refatorando o código para deixar mais legível

--- ex004.js
+++ ex004.js
@@ -18,30 +18,13 @@ const rl = readline.createInterface({
 });
 
 function tabuada() {
-    console.log(`
-    =-=-= TABUADA =-=-=
-      1 - Tabuada do 1
-      2 - Tabuada do 2
-      3 - Tabuada do 3
-      4 - Tabuada do 4
-      5 - Tabuada do 5
-      6 - Tabuada do 6
-      7 - Tabuada do 7
-      8 - Tabuada do 8
-      9 - Tabuada do 9
-      10 - Tabuada do 10
-        `);
-
     rl.question('Escolha um número para ver a tabuada entre 1 e 10: ', (escolha) => {
         const numero = Number(escolha);
         for (let i = 1; i <= 10; i++) {
-            const result = escolha * i;
-            console.log(`${escolha} * ${i} = ${result}`);
+            const result = numero * i;
+            console.log(`${numero} * ${i} = ${result}`);
         }
+        rl.close();
     })
-
 }
-
-tabuada();
-
-            
+tabuada();            
